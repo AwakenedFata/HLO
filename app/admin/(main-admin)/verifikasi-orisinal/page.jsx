4cc4e1d97e74ef565d245c2c6621cb7895b237e3
AwakenedFata <noroinoogmg@gmail.com>
2025-11-15 23:14:18 +0700

    wow

---
+++ app/admin/(main-admin)/verifikasi-orisinal/page.jsx
@@ -0,0 +1,8 @@
+"use client"
+import VerifikasiOrisinalManagement from "@/components/pages/admin/VerifikasiOrisinalManagement";
+
+export const dynamic = "force-dynamic";
+
+export default function VerifikasiOrisinalPage() {
+  return <VerifikasiOrisinalManagement/>;
+}
