b71126643c8daf59ea6dd29eabb771527030add3
AwakenedFata <noroinoogmg@gmail.com>
2025-11-15 23:16:53 +0700

    Stop tracking verifikasi and sensitive files

--- app/admin/(main-admin)/verifikasi-orisinal/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import VerifikasiOrisinalManagement from "@/components/pages/admin/VerifikasiOrisinalManagement";
-
-export const dynamic = "force-dynamic";
-
-export default function VerifikasiOrisinalPage() {
-  return <VerifikasiOrisinalManagement/>;
-}
